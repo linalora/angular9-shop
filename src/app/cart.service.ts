08964a3e681835f6fc221e9cf772fdd39caebac5
“Lina <lina.lora@gmail.com>
2020-09-08 17:59:07 -0500

    first commit

---
+++ src/app/cart.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable()
+export class CartService {
+  items = [];
+  constructor(
+    private http: HttpClient
+  ) {}
+  public addToCart(product) {
+    this.items.push(product);
+  }
+
+  public getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+}
